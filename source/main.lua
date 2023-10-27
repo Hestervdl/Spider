977f1f9109e18494b747529ea162a1d8d81fc428
hester <hesterhvdlaan@gmail.com>
2023-10-27 14:37:57 +0200

    ball

--- source/main.lua
+++ source/main.lua
@@ -1,11 +1,18 @@
 import "CoreLibs/graphics"
 import "corelibs/object"
 import "CoreLibs/sprites"
+import "circle"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
+local function initialize()
+    local circleSprite = Circle(200,120,20)
+    circleSprite:add()
+end
+
+initialize()
+
 function playdate.update() 
-    gfx.clear()
-    gfx.drawText("Hello World", 20, 20)
+    gfx.sprite.update()
 end
