4b35e5d1d904aa929c2374d99681e3cdfac35b1c
hester <hesterhvdlaan@gmail.com>
2023-10-27 15:50:28 +0200

    updownrightleft spider

--- source/main.lua
+++ source/main.lua
@@ -1,18 +1,15 @@
 import "CoreLibs/graphics"
 import "corelibs/object"
 import "CoreLibs/sprites"
-import "circle"
+import "corelibs/timer"
+import "player"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-local function initialize()
-    local circleSprite = Circle(200,120,20)
-    circleSprite:add()
-end
-
-initialize()
+player(50,120)
 
 function playdate.update() 
     gfx.sprite.update()
+    pd.timer.updateTimers()
 end
---
+++ source/player.lua
@@ -0,0 +1,32 @@
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+class('player').extends(gfx.sprite)
+
+function player:init(x,y)
+    local playerImage = gfx.image.new("images/spider")
+    self:setImage(playerImage)
+    self:moveTo(x,y)
+    self:add()
+    self.Speed = 3
+end
+
+function player:update()
+    if pd.buttonIsPressed(pd.kButtonUp) then
+        if self.y > 0 then
+            self:moveBy(0,-self.Speed)
+        end
+    elseif pd.buttonIsPressed(pd.kButtonDown) then
+        if self.y < 240 then
+            self:moveBy(0,self.Speed)
+        end
+    elseif pd.buttonIsPressed(pd.kButtonRight) then
+        if self.x < 360 then
+            self:moveBy(self.Speed,0)
+        end
+    elseif pd.buttonIsPressed(pd.kButtonLeft) then
+        if self.x > 0 then
+            self:moveBy(-self.Speed,0)
+        end
+    end
+end
